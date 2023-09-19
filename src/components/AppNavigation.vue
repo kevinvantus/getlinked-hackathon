773cdde6212e9024314114b0389089e187cb87d0
Kevin Vantus <kevinvantus@gmail.com>
2023-09-19 17:48:34 +0100

    added main navigation

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,94 @@
+<template>
+  <nav>
+    <RouterLink to="/">
+      <div class="logo">
+        <span class="white">get</span>
+        <span class="pink">linked</span>
+      </div>
+    </RouterLink>
+    <button type="button">
+      <IconMenu />
+    </button>
+    <ul>
+      <li><RouterLink to="/">Timeline</RouterLink></li>
+      <li><RouterLink to="/">Overview</RouterLink></li>
+      <li><RouterLink to="/">FAQs</RouterLink></li>
+      <li><RouterLink to="/contact">Contact</RouterLink></li>
+      <li><RouterLink to="/register" class="main">Register</RouterLink></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import IconMenu from "./icons/IconMenu.vue";
+
+export default {
+  name: "app-navigation",
+  components: {
+    IconMenu,
+  },
+};
+</script>
+
+<style>
+nav {
+  display: flex;
+  align-items: center;
+  padding: 1.75rem 3rem 1.3125rem;
+  border-block-end: 1px solid #ffffff2e;
+}
+
+nav .logo {
+  font-family: var(--font-secondary);
+  line-height: 1.2;
+  color: #fff;
+}
+
+nav .logo span {
+  font-weight: 700;
+}
+
+nav .logo .pink {
+  color: var(--color-primary);
+}
+
+nav button {
+  margin-inline-start: auto;
+  background: none;
+  border: none;
+  outline: 0;
+}
+
+nav ul {
+  display: none;
+}
+
+@media screen and (min-width: 26.25rem) {
+  .logo {
+    font-size: 1.25rem;
+  }
+}
+
+@media screen and (min-width: 62rem) {
+  nav .logo {
+    font-size: 2.25rem;
+    line-height: 1.2;
+    color: #fff;
+  }
+
+  nav button {
+    display: none;
+  }
+
+  nav ul {
+    margin-inline-start: auto;
+    color: #fff;
+    display: flex;
+    gap: 3.5rem;
+  }
+
+  nav ul li:last-of-type {
+    margin-inline-start: 4.0625rem;
+  }
+}
+</style>
